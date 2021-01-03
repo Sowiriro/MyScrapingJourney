3f14138a01980b47aa9cd026a26e46ae3fb34ee0
Sowiriro <sowiriro@gmail.com>
2021-01-03 20:31:51 +0900

    another scraping from starbucks and deal with text garbling

--- crowler.py
+++ crowler.py
@@ -1,8 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
  
 url = "https://product.starbucks.co.jp/goods/tumbler/4524785332171/?category=goods%2Ftumbler"
  
 response = requests.get(url)
 
- 
+soup = BeautifulSoup(response.content,  'html.parser')
+
+elems = soup.find_all("p", class_="notification")
+for e in elems:
+    print(e.getText())
+
 print(response.url)
